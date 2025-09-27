f83433ca68b716daac0f1941ccb7b416497bb08a
NaufalArdian12 <naufalopo87@gmail.com>
2025-09-27 21:13:15 +0700

    feat: add theme

---
+++ lib/app/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final _seed = const Color(0xFF3278FF);
+  static ThemeData get light => ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: _seed,
+          brightness: Brightness.light,
+        ),
+      );
+  static ThemeData get dark => ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: _seed,
+          brightness: Brightness.dark,
+        ),
+      );
+}
